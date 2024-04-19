da3ab1f3492a1f0013da0d7e8f1bfbc5843ee076
RenzoZumaeta98 <renzozumaeta98@gmail.com>
2024-04-19 11:08:18 -0500

    Subiendo Proyecto

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(); //Si esta vacio, busca la vista con el nombre de la funcion ("Index()")
+                            //Si se quiere buscar otra vista, colocar View("NombreDeVista")
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
